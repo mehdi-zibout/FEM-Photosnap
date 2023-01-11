c089cb06f98084c2697704e9e869b105f3fe5a16
Mehdi Zibout <mehdizibout@gmail.com>
2023-01-11 15:32:49 +0100

    Add Navbar

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,10 @@
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
+    screens: {
+      desktop: "1200px",
+      tablet: "720px",
+    },
     colors: {
       black: "#000",
       "light-grey": "#DFDFDF",
